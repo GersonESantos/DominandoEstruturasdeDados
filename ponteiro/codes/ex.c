a2f20bc41713be1d32ee6fef142c4768bfc528a3
GersonESS <gersones@outlook.com>
2024-08-05 00:47:19 -0300

    a

--- ponteiro/codes/ex.c
+++ ponteiro/codes/ex.c
@@ -4,16 +4,38 @@
 
 int main() {
     int a, b, *p1, *p2;
-
+    p1 = NULL;
+    p2 = NULL;
+    
+    printf("--------------Teste De Mesa-------------\n");
+    printf("a  b       p1  *p2 p2       *p2 -----------------\n");
+    //printf("             %d     %d\n", *p1, *p2);
+    printf("#  #       #    -   #       -\n");
     a = 4;
+  
+    printf("%d  #       #    -   #       -\n", a);
+ 
     b = 3;
+    printf("%d " " %d       #    -   #\n", a, b);
     p1 = &a;
+    printf("%d  %d  %p  -   #       -\n", a, b, &p1);
+    printf("%d  %d  %p  %d   #      -\n", a, b, &p1, *p1);
     p2 = p1;
-    *p2 = *p1 + 3; b = b * (*p1); (*p2)++;
+    printf("%d  %d  %p  %d   %p     %d\n", a, b, &p1, *p1, &p2, *p2);
+    *p2 = *p1 + 3;
+    printf("%d  %d  %p  %d   %p     %d\n", a, b, &p1, *p1, &p2, *p2);
+     b = b * (*p1);
+     printf("%d  %d  %p  %d   %p     %d\n", a, b, &p1, *p1, &p2, *p2);
+    (*p2)++;
+    printf("%d  %d  %p  %d   %p     %d\n", a, b, &p1, *p1, &p2, *p2);
     p1 = &b;
+     
+    printf("%d  %d  %p  %d   %p     %d\n", a, b, &p1, *p1, &p2, *p2);
+    printf("---------------------------------------------\n");
+   printf("%d %d\n", *p1, *p2);
 
-    printf("%d %d\n", *p1, *p2);
-    printf("%d %d\n", a, b);
+
+    printf("%d  %d\n", a, b);
 
     return 0;
 }
